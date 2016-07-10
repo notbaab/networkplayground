4c9c8f5e267540e519ece6a28759aa37e66bed4d
Erik Parreira <etinlb@gmail.com>
2016-07-10 10:11:44 -0700

    Added game object registry
    The registry keeps track of how to spawn game objects and adds them to the given game world

---
+++ networkplayground/include/gameobjects/GameObjectRegistry.h
@@ -0,0 +1,45 @@
+//
+//  GameObjectRegistry.h
+//  networkplayground
+//
+//  Created by Erik Parreira on 7/7/16.
+//  Copyright © 2016 Erik Parreira. All rights reserved.
+//
+
+#ifndef GameObjectRegistry_h
+#define GameObjectRegistry_h
+
+#include <stdio.h>
+#include <memory>
+#include <unordered_map>
+
+#include "gameobjects/GameObject.h"
+
+typedef GameObjectPtr ( *GameObjectCreationFunc )();
+
+typedef void (* addToWorldFunction)(GameObjectPtr);
+
+class GameObjectRegistry
+{
+public:
+    // Takes a function that should be called after the object is created
+    static void StaticInit( addToWorldFunction worldAddFunction );
+    
+    // Singleton instance
+    static std::unique_ptr< GameObjectRegistry > sInstance;
+    
+    void RegisterCreationFunction( uint32_t inCCName,
+                                   GameObjectCreationFunc inCreationFuntion );
+    
+    GameObjectPtr CreateGameObject ( uint32_t inCCName );
+    
+private:
+    GameObjectRegistry();
+    
+    addToWorldFunction mAddToWorldFunction;
+    
+    std::unordered_map< uint32_t, GameObjectCreationFunc > mNameToCreationFunction;
+    
+};
+
+#endif /* GameObjectRegistry_h */
---
+++ networkplayground/src/gameobjects/GameObjectRegistry.cpp
@@ -0,0 +1,38 @@
+//
+//  GameObjectRegistry.cpp
+//  networkplayground
+//
+//  Created by Erik Parreira on 7/7/16.
+//  Copyright © 2016 Erik Parreira. All rights reserved.
+//
+
+#include "gameobjects/GameObjectRegistry.h"
+
+
+std::unique_ptr < GameObjectRegistry > GameObjectRegistry::sInstance;
+
+void GameObjectRegistry::StaticInit( addToWorldFunction inAddToWorld )
+{
+    // Reset pointer to new instance of game object registry
+    sInstance.reset( new GameObjectRegistry() );
+    sInstance->mAddToWorldFunction = inAddToWorld;
+}
+
+GameObjectRegistry::GameObjectRegistry()
+{}
+
+GameObjectPtr GameObjectRegistry::CreateGameObject( uint32_t inCCName )
+{
+    GameObjectPtr newObject = sInstance->mNameToCreationFunction[inCCName]();
+    
+    sInstance->mAddToWorldFunction( newObject );
+    
+    return newObject;
+}
+
+void GameObjectRegistry::RegisterCreationFunction( uint32_t inCCName,
+                                                   GameObjectCreationFunc inCreationFuntion )
+{
+    sInstance->mNameToCreationFunction[inCCName] = inCreationFuntion;
+}
+
