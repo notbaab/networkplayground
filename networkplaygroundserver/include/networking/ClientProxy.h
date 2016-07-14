e729e4441017a60495d2ce0911a3cf44cf0e3603
Erik Parreira <etinlb@gmail.com>
2016-07-14 09:34:49 -0700

    Sketched serverside client representation

---
+++ networkplaygroundserver/include/networking/ClientProxy.h
@@ -0,0 +1,34 @@
+//
+//  ClientProxy.h
+//  networkplayground
+//
+//  Created by Erik Parreira on 7/13/16.
+//  Copyright © 2016 Erik Parreira. All rights reserved.
+//
+
+#ifndef ClientProxy_h
+#define ClientProxy_h
+
+#include <string>
+
+#include "networking/SocketAddress.h"
+
+class ClientProxy
+{
+public:
+    ClientProxy( const SocketAddress& inSocketAddres, const std::string& inName,
+                 int inPlayerId );
+    
+    const SocketAddress& GetSocketAddress() const { return mSocketAddress; }
+
+private:
+    SocketAddress mSocketAddress;
+    std::string mName;
+    int mPlayerId;
+    
+    float mLastPacketFromClientTime;
+    
+//    MoveList mUnprocessedMoveList;
+};
+
+#endif /* ClientProxy_h */
---
+++ networkplaygroundserver/src/networking/ClientProxy.cpp
@@ -0,0 +1,15 @@
+//
+//  ClientProxy.cpp
+//  networkplayground
+//
+//  Created by Erik Parreira on 7/13/16.
+//  Copyright © 2016 Erik Parreira. All rights reserved.
+//
+
+#include "networking/ClientProxy.h"
+
+ClientProxy::ClientProxy( const SocketAddress& inSocketAddress,
+                          const std::string& inName, int inPlayerId )
+{
+    
+}
