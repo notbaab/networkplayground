5c674ccfb68996a7186917bc0f5ff41b31e0e40d
Erik Parreira <etinlb@gmail.com>
2016-07-18 09:51:53 -0700

    MoveList, the move container

---
+++ networkplayground/include/gameobjects/MoveList.h
@@ -0,0 +1,41 @@
+//
+//  MoveList.h
+//  networkplayground
+//
+//  Created by Erik Parreira on 7/17/16.
+//  Copyright © 2016 Erik Parreira. All rights reserved.
+//
+
+#ifndef MoveList_h
+#define MoveList_h
+
+#include "gameobjects/Move.h"
+#include <deque>
+
+class MoveList
+{
+  public:
+    typedef std::deque<Move>::const_iterator const_iterator;
+    typedef std::deque<Move>::const_reference const_reverse_iterator;
+
+    MoveList() : mLastMoveTimestamp( -1.f ) {}
+
+    const Move& AddMove( const InputState& inInputState, float inTimeStamp );
+    bool AddMoveIfNew( const Move& inMove );
+    void RemoveProcessedMoves( float inLAstMoveProcessedTimestamp );
+
+    float GetLastMoveTimestamp() const { return mLastMoveTimestamp; }
+    const Move& GetLatestmove() const { return mMoves.back(); }
+    void Clear() { mMoves.clear(); }
+    bool HasMoves() const { return !mMoves.empty(); }
+    int GetMoveCount() const { return (int)mMoves.size(); }
+
+    const_iterator begin() const { return mMoves.begin(); }
+    const_iterator end() const { return mMoves.end(); }
+
+  private:
+    float mLastMoveTimestamp;
+    std::deque<Move> mMoves;
+};
+
+#endif /* MoveList_h */
---
+++ networkplayground/src/gameobjects/MoveList.cpp
@@ -0,0 +1,50 @@
+//
+//  MoveList.cpp
+//  networkplayground
+//
+//  Created by Erik Parreira on 7/17/16.
+//  Copyright © 2016 Erik Parreira. All rights reserved.
+//
+
+#include "gameobjects/MoveList.h"
+
+const Move& MoveList::AddMove( const InputState& inInputState,
+                               float inTimeStamp )
+{
+    float deltaTime =
+        mLastMoveTimestamp >= 0.f ? inTimeStamp - mLastMoveTimestamp : 0.f;
+
+    mMoves.emplace_back( inInputState, inTimeStamp, deltaTime );
+    mLastMoveTimestamp = inTimeStamp;
+
+    return mMoves.back();
+}
+
+bool MoveList::AddMoveIfNew( const Move& inMove )
+{
+    float timeStamp = inMove.GetTimeStamp();
+
+    // only add if latter than current move
+    if ( timeStamp <= mLastMoveTimestamp )
+    {
+        return false;
+    }
+
+    float deltaTime =
+        mLastMoveTimestamp >= 0.f ? timeStamp = mLastMoveTimestamp : 0.f;
+
+    mLastMoveTimestamp = timeStamp;
+
+    mMoves.emplace_back( inMove.GetInputState(), timeStamp, deltaTime );
+    return true;
+}
+
+void MoveList::RemoveProcessedMoves( float inLAstMoveProcessedTimestamp )
+{
+    // remove moves until we are up to the current time stamp
+    while ( !mMoves.empty() &&
+            mMoves.front().GetTimeStamp() <= inLAstMoveProcessedTimestamp )
+    {
+        mMoves.pop_front();
+    }
+}
