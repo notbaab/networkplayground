5779c961b5ff7dd1408543fa916b66072ae7fd5b
Erik Parreira <etinlb@gmail.com>
2017-08-06 18:11:43 -0700

    Math namespace random module

---
+++ networkplayground/include/math/Random.h
@@ -0,0 +1,4 @@
+namespace Math
+{
+    float GetRandomFloat();
+}
---
+++ networkplayground/src/math/Random.cpp
@@ -0,0 +1,10 @@
+#include "math/Random.h"
+#include <random>
+
+float Math::GetRandomFloat()
+{
+    static std::random_device rd;
+    static std::mt19937 gen( rd() );
+    static std::uniform_real_distribution< float > dis( 0.f, 1.f );
+    return dis( gen );
+}
