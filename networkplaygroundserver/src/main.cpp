69475238181b79f739f18b6685cedd5fcc65fd88
Erik Parreira <etinlb@gmail.com>
2018-08-30 21:57:40 -0700

    Start of a very simple console for the server

--- networkplaygroundserver/src/main.cpp
+++ networkplaygroundserver/src/main.cpp
@@ -1,42 +1,50 @@
 #include <iostream>
-#include <thread>
 #include <string>
+#include <thread>
 
-#include "networking/Server.h"
-#include "networking/Logger.h"
+#include "console.h"
 #include "gameobjects/World.h"
+#include "networking/Logger.h"
+#include "networking/Server.h"
 
 // const int port = 3200;
 
 #define EXIT "exit"
 // Some basic setup.
 
-void interactive_console()
-{
-    setbuf( stdout, NULL );
-    std::string command;
-    do {
-        std::cin >> command;
-
-        if ("locate" == command)
-        {
-
-            World::PrintInfo();
-        }
-    } while (EXIT != command);
-    std::cout << "Exiting";
-}
-
 const char** __argv;
 int __argc;
 int main( int argc, const char* argv[] )
 {
-    std::thread t(&interactive_console);   // t starts running
+    std::string log_file;
+    std::thread t;
+    while ( argc > 1 )
+    {
+        argc--;
+        argv++;
+        if ( !strcmp( *argv, "--console" ) )
+        {
+            std::cout << "Starting a shell" << std::endl;
+            t = std::thread( &interactive_console );
+            // std::thread t(&interactive_console);   // t starts running
+        }
+        else if ( !strcmp( *argv, "--log-file" ) )
+        {
+            argv++;
+            argc--;
+            log_file = *argv;
+        }
+    }
 
-    Logger::InitLog(spdlog::level::trace, "server");
-    INFO("Fucking start");
-    TRACE("Fucking start");
-    DEBUG("Stuff")
+    // Logger::InitLog(spdlog::level::info, "server");
+    if ( !log_file.empty() )
+    {
+        Logger::InitLog( spdlog::level::info, "server", log_file );
+    }
+    else
+    {
+        Logger::InitLog( spdlog::level::info, "server");
+    }
 
     if ( Server::StaticInit() )
     {
